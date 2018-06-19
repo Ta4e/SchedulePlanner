e09704bda330c5fbdc47740ca6afc4e3deaef45c
Stanislav <39307406+Ta4e@users.noreply.github.com>
2018-06-19 23:39:49 +0500

    loginBox

---
+++ frontPage.js
@@ -0,0 +1,15 @@
+//LoginBox
+
+// function logBox() {
+// 	// let header = document.getElementById('headerBox');
+// 	// let box; //this is a string for box
+// 	// let par = document.createElement("p");
+// 	// let	messageBox = document.createTextNode("Test login box");
+// 	// par.appendChild(messageBox);
+
+// 	// let butt = document.getElementById('login');
+// 	alert('test');
+
+// }
+// let logInBox = document.getElementById("login");
+// logInBox.addEventListener("click", logBox);
