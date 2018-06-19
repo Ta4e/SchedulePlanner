b916e2e7481c77a6ed71898dd07656944556bfbf
Stanislav <39307406+Ta4e@users.noreply.github.com>
2018-06-20 00:34:21 +0500

    login box
    adding forms for loginBox in js

--- frontPage.js
+++ frontPage.js
@@ -1,15 +1,22 @@
 //LoginBox
-
-// function logBox() {
-// 	// let header = document.getElementById('headerBox');
-// 	// let box; //this is a string for box
-// 	// let par = document.createElement("p");
-// 	// let	messageBox = document.createTextNode("Test login box");
-// 	// par.appendChild(messageBox);
-
-// 	// let butt = document.getElementById('login');
-// 	alert('test');
-
-// }
-// let logInBox = document.getElementById("login");
-// logInBox.addEventListener("click", logBox);
+window.onload = function() {
+function logBox() {
+	let header = document.getElementById('headerBox');
+	let par = document.createElement("p");
+	let aPar = document.createElement("p");
+	let	messageBox = document.createTextNode("Login: ");
+	let messageBoxP = document.createTextNode("Password: ");
+	let testButton = document.createElement("input");
+	let testButtonP = document.createElement("input");
+	let submit = document.createElement("inputTypeSubmit");
+	par.appendChild(messageBox);
+	par.appendChild(testButton);
+	aPar.appendChild(messageBoxP);
+	aPar.appendChild(testButtonP);
+	par.appendChild(aPar);
+	par.appendChild(submit);
+	header.appendChild(par);
+}
+let logInBox = document.getElementById("login");
+logInBox.addEventListener("click", logBox);
+};
