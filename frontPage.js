2957c1c880bd26b44bd27cf9eaff6d9db170043b
Stanislav <39307406+Ta4e@users.noreply.github.com>
2018-06-23 13:44:11 +0300

    login can be removed now

--- frontPage.js
+++ frontPage.js
@@ -1,37 +1,34 @@
 //LoginBox
 window.onload = function() {
-	let exist;
 function logBox() {
 	let header = document.getElementById('headerBox');
 	let diva = document.createElement("div");
-		diva.id = "diva";
 	let par = document.createElement("p");
 	let aPar = document.createElement("p");
-	let	messageBox = document.createTextNode("Login: ");
-	let messageBoxP = document.createTextNode("Password: ");
-	let testButton = document.createElement("input");
-	let testButtonP = document.createElement("input");
+	let	loginText = document.createTextNode("Login: ");
+	let passText = document.createTextNode("Password: ");
+	let loginBox = document.createElement("input");
+	let passBox = document.createElement("input");
 	let submit = document.createElement("input");
 		submit.setAttribute("type", "submit");
 		submit.setAttribute("value", "Send");
-	par.appendChild(messageBox);
-	par.appendChild(testButton);
-	aPar.appendChild(messageBoxP);
-	aPar.appendChild(testButtonP);
+		submit.id = 'remover';
+	par.appendChild(loginText);
+	par.appendChild(loginBox);
+	aPar.appendChild(passText);
+	aPar.appendChild(passBox);
 	par.appendChild(aPar);
 	par.appendChild(submit);
 	diva.appendChild(par);
-	exist = diva;
 	header.appendChild(diva);
-}
-function logBoxCut() { //does not work
-	document.getElementById("diva").remove();
-	let header = document.getElementById('headerBox');
-	header.removeChild(exist);
+let remover = document.getElementById('remover');
+remover.addEventListener("click", logBoxCut);
+	function logBoxCut() { 
+		let remover = document.getElementById('remover');
+		remover.addEventListener("click", header.removeChild(diva));
+		header.removeChild(header.lastNode);
+	}
 }
 let logInBox = document.getElementById("login");
-logInBox.addEventListener("click", logBoxCut);
 logInBox.addEventListener("click", logBox);
-test.addEventListener("click", header.removeChild(exist));
 };
-//Workin on delete diva after submit press down!
