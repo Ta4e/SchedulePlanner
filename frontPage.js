3dcaf14fe5d15876cf387a71da4f64702bd1fd0e
Stanislav <39307406+Ta4e@users.noreply.github.com>
2018-06-24 20:41:21 +0300

    fixed multiple adds of login box
    Now it doesn't

--- frontPage.js
+++ frontPage.js
@@ -1,32 +1,38 @@
 //LoginBox
 window.onload = function() {
+	let count = 0;
 function logBox() {
+	let countIn = count;
 	let header = document.getElementById('headerBox');
 	let diva = document.createElement("div");
 	let par = document.createElement("p");
 	let aPar = document.createElement("p");
-	let	loginText = document.createTextNode("Login: ");
-	let passText = document.createTextNode("Password: ");
+	let	loginText = document.createTextNode("  :Log");
+	let passText = document.createTextNode("  :Pas");
 	let loginBox = document.createElement("input");
 	let passBox = document.createElement("input");
 	let submit = document.createElement("input");
 		submit.setAttribute("type", "submit");
 		submit.setAttribute("value", "Send");
 		submit.id = 'remover';
-	par.appendChild(loginText);
-	par.appendChild(loginBox);
-	aPar.appendChild(passText);
-	aPar.appendChild(passBox);
-	par.appendChild(aPar);
-	par.appendChild(submit);
-	diva.appendChild(par);
-	header.appendChild(diva);
-let remover = document.getElementById('remover');
-remover.addEventListener("click", logBoxCut);
+	if (count == 0) {
+		count++;
+		par.appendChild(loginBox);
+		par.appendChild(loginText);
+		aPar.appendChild(passBox);
+		aPar.appendChild(passText);
+		par.appendChild(aPar);
+		par.appendChild(submit);
+		diva.appendChild(par);
+		header.appendChild(diva);
+	let remover = document.getElementById('remover');
+	remover.addEventListener("click", logBoxCut);
+	}
 	function logBoxCut() { 
+		count--;
 		let remover = document.getElementById('remover');
 		remover.addEventListener("click", header.removeChild(diva));
-		header.removeChild(header.lastNode);
+		header.removeChild(header.diva);
 	}
 }
 let logInBox = document.getElementById("login");
