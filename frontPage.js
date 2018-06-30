6e6ab2bfa143b570831ce1489e555e724a956084
Stanislav <39307406+Ta4e@users.noreply.github.com>
2018-06-30 23:59:26 +0300

    add

--- frontPage.js
+++ frontPage.js
@@ -4,7 +4,7 @@ let u1Day = ['01', '03', 11, '07', 16];
 let u2Day = [11, '06', 17];
 let user1 = new user('Stanislav','Smirnov','Nikolaevich','admin','admin', dayArr(u1Day));
 let user2 = new user('Evgenia', 'Kozhukhova', 'Aleksandrovna', 'example', 'example', dayArr(u2Day));
-let user2 = new user('Test', 'Test');
+let user3 = new user('Test', 'Test');
 function dayArr(usable) {
 	day = usable;
 return day;
