1bbcc0c1fa7d4bd1f71244daf2b33d64ccf58b95
Stanislav <39307406+Ta4e@users.noreply.github.com>
2018-07-08 02:31:45 +0300

    done login screen

--- frontPage.js
+++ frontPage.js
@@ -58,46 +58,6 @@ $("#crewListHead").click(function() {
 	});
 //.show and .hide END
 /**/
-//LoginBox START
-	let count = 0;
-	function logBox() {
-		let countIn = count;
-		let header = document.getElementById('headerBox');
-		let diva = document.createElement("div");
-		let par = document.createElement("p");
-		let aPar = document.createElement("p");
-		let	loginText = document.createTextNode("  :Log");
-		let passText = document.createTextNode("  :Pas");
-		let loginBox = document.createElement("input");
-		let passBox = document.createElement("input");
-		let submit = document.createElement("input");
-			submit.setAttribute("type", "submit");
-			submit.setAttribute("value", "Send");
-			submit.id = 'remover';
-		if (count == 0) {
-			count++;
-			par.appendChild(loginBox);
-			par.appendChild(loginText);
-			aPar.appendChild(passBox);
-			aPar.appendChild(passText);
-			par.appendChild(aPar);
-			par.appendChild(submit);
-			diva.appendChild(par);
-			header.appendChild(diva);
-			let remover = document.getElementById('remover');
-			remover.addEventListener("click", logBoxCut);
-		}
-		function logBoxCut() { 
-			count--;
-			let remover = document.getElementById('remover');
-			remover.addEventListener("click", header.removeChild(diva));
-			header.removeChild(header.diva);
-		}
-	}
-	let logInBox = document.getElementById("login");
-	logInBox.addEventListener("click", logBox);
-//LoginBox END
-/**/
 //.calCell dynamic highlight START
 	$('.users').mouseenter(function(){
 	  	let uIdFinder = this.id;
